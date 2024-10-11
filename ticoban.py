2bcaa759468b1626d0874e2dd0b228c0dce631ff
Alfonso Saavedra "Son Link <sonlink.dourden@gmail.com>
2024-10-11 23:22:18 +0200

    Added metadata to the startup script

--- ticoban.py
+++ ticoban.py
@@ -1,3 +1,9 @@
+# title: Ticoban
+# author: Alfonso Saavedra "Son Link"
+# desc:  A Sobokan clone make with Pyxel
+# site: https://github.com/son-link/ticoban-pyxel
+# license: GNU General Public License v3.0
+# version: 0.2.0
 from game import Ticoban
 
 Ticoban()
